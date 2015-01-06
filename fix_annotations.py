cabddd8cebc6b9c1a044b58f37d5fae7f5bdacac
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-01-06 12:17:39 +0400

    Write recursive version of fix_annotations
    fix_annotations_recursive can take care of nested classes. It now
    needs some test cases.

--- fix_annotations.py
+++ fix_annotations.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
 import json
+import inspect
 
 __author__ = "Dibyo Majumdar"
 __email__ = "dibyo.majumdar@gmail.com"
 
 
 def fix_annotations(cls: object):
+    # Get the class name
     cls_name = cls.__name__
 
-    for attr_name in cls.__dict__:
-        # Get all attributes of the class
-        attr = getattr(cls, attr_name)
-
+    # Check all attributes of the class
+    for attr in cls.__dict__.values():
         # Check if the attribute is a function (ie. if it is callable) and
         # if it has stored annotations.
-        if callable(attr) and hasattr(attr, '__annotations__'):
+        if inspect.isfunction(attr):
             for param, annotation in attr.__annotations__.copy().items():
                 if annotation == cls_name:
                     attr.__annotations__[param] = cls
@@ -23,7 +23,49 @@ def fix_annotations(cls: object):
     return cls
 
 
-@fix_annotations
+#####################
+# Recursive version #
+#####################
+
+def get_cls_names(cls: object) -> list:
+    prefix = cls.__name__
+    cls_names = {prefix: cls}
+
+    for attr in cls.__dict__.values():
+        if inspect.isclass(attr):
+            nested_cls_names = get_cls_names(attr)
+
+            for name, nested_class in nested_cls_names:
+                cls_names['.'.join([prefix, name])] = nested_class
+
+    return cls_names
+
+
+def check_annotations(cls: object, cls_names: list) -> cls:
+    for attr_name in cls.__dict__:
+        attr = getattr(cls, attr_name)
+
+        if inspect.isclass(attr):
+            check_annotations(attr, cls_names)
+        elif inspect.ismethod(attr) or inspect.isfunction(attr):
+            for param, annotation in attr.__annotations__.copy().items():
+                if annotation in cls_names:
+                    attr.__annotations__[param] = cls_names[annotation]
+
+
+def fix_annotations_recursive(cls: object):
+    cls_names = get_cls_names(cls)
+
+    check_annotations(cls, cls_names)
+
+    return cls
+
+
+###########
+# Testing #
+###########
+
+@fix_annotations_recursive
 class Test(object):
     """
     Just a test class with just random stuff that hopefully demonstrate
@@ -63,7 +105,7 @@ class Test(object):
     @greeting.deleter
     def greeting(self):
         del self._greeting
-    
+
     def to_json(self) -> str:
         """
         Encode to JSON string.
